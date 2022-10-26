0ae6e0bcb99d8ba548305dbd4b740f920763004d
kiranmai17 <116304133+kiranmai17@users.noreply.github.com>
2022-10-26 21:01:15 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,17 @@
+'''
+DATE=26th oct 2022
+DAY= wednesday
+TOPIC= strings 
+AUTHOR=Kiranmai
+'''
+print(type('kiranmai'))
+print(type('3'))
+print(type('0.235'))
+a=4
+print(str(a )+'+b')
+print(str(a))
+print('')      #empty
+print("")
+print('kiranmai\n' )
+print('stella')
+print('stella','kiran')
